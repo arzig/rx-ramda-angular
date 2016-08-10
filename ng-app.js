d0eaaaba25037db3ca4477ee02ce34c3024629b9
Ryan Zeigler <ryan.zeigler@taillight.com>
2016-08-10 09:43:24 -0500

    Improve ng example.
    Shows simple lenses in context.

--- ng-app.js
+++ ng-app.js
@@ -7,13 +7,20 @@ const bindTo = R.curry((path, values$, model) => values$.subscribe(v => {
     model[path] = v;
 }));
 
-const callToStream = R.curry((name, target) => {
+const callbackToStream = R.curry((name, target) => {
     const subject = new Subject();
     target[name] = function () {
         subject.next(Array.prototype.slice.call(arguments, 0));
     }
     return subject;
 });
+
+const watchStream = R.curry((expr, $scope) =>
+    Observable.create((observer) =>
+        // Return the $scope expression which can be used as teardown logic
+        $scope.$watch(expr, (nv, ov) => observer.next(nv))
+    )
+);
 
 const unsubscriber = (subscription) => () => {
     subscription.unsubscribe();
@@ -42,18 +49,36 @@ function stateStream(startValue) {
     };
 }
 
+const valueLens = R.lensProp("value");
+const textLens = R.lensProp("text");
+
 angular.module("StateApp", [])
     .controller("StateController", ["$scope", function StateController($scope) {
-        const state = stateStream(0);
+        const state = stateStream({value: 0, text: ""});
 
-        const inc$ = callToStream('inc', $scope).map(R.always(R.add(1)));
-        const dec$ = callToStream('dec', $scope).map(R.always(R.add(-1))); // -1 is easier than subtract
-        const reset$ = callToStream('reset', $scope).map(R.always(R.always(0))); // Always return a function that returns 0
+        const inc$ = callbackToStream('inc', $scope).map(R.always(R.over(valueLens, R.add(1))));
+        const dec$ = callbackToStream('dec', $scope).map(R.always(R.over(valueLens, R.add(-1)))); // -1 is easier than subtract
+        const reset$ = callbackToStream('reset', $scope)
+            .map(R.always(R.compose(
+                R.set(valueLens, 0),
+                R.set(textLens, "")
+            )));
+
+        const setText$ = watchStream('text', $scope)
+            .filter(R.identity)
+            .map(t => R.set(textLens, t)); // .map(R.set(textLens)) also works
+
+        const bufferedText$ = state.value$.map(R.view(textLens))
+            .distinct(R.equals)
+            .merge(reset$.map(R.always(false)))
+            .scan((acc, v) => v ? R.prepend(v, R.take(2, acc)) : [], []);
 
         $scope.inc5 = () => {
-            state.update(R.add(5));
+            state.update(R.over(valueLens, R.add(5)));
         }
 
-        bindTo('value', state.value$, $scope);
-        state.updateWith(inc$.merge(dec$).merge(reset$));
+        bindTo('value', state.value$.map(R.view(valueLens)), $scope);
+        bindTo('bufferedText', bufferedText$, $scope);
+
+        state.updateWith(inc$.merge(dec$).merge(reset$).merge(setText$));
     }]);
